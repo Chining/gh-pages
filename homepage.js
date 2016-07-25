25f54c4d372c597128ed077fc25647400fe28d9a
Chining <chining.hwang@gmail.com>
2016-07-25 16:51:50 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,3 @@
+document.getElementById("foot01").innerHTML = "<p>Copyright &copy; 2016</p> <p>All rights reserved by Linda Yang. For personal use only.</p>";
+
+document.getElementById("nav01").innerHTML = "<ul id-'menu>" + "<li><a href='index.html'>Home</a></li>" + "<li><a href='customers.html'>Data</a></li>" + "<li><a href='about.html'>About</a></li>" + "</ul>";
